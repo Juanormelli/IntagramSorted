4818f5dc0b347bf08e5313b5a7cc871772c8bef2
Juan Ormelli <juan_ormelli@hotmail.com>
2020-07-30 21:47:56 -0300

    commit

---
+++ project.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+const fakeCommits = ["juan","julio","jose","joao","juan"]
+
+function count(commits){
+    const counter=Object.assign(commits)
+    return counter
+           
+        
+    
+}
+
+function randomWinner(count){
+    const randomValue=count[Math.floor(count.length*Math.random())]
+    return randomValue
+
+    
+}
+console.log( randomWinner( count(fakeCommits)))
