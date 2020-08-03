e80a65d2367c306fcff1d5765af7d50a6e1a62a8
Juan Ormelli <67693801+Juanormelli@users.noreply.github.com>
2020-08-03 19:54:48 -0300

    Add files via upload
    Initial implementation front-end

--- project.js
+++ project.js
@@ -1,9 +1,43 @@
+const puppeteers= require("puppeteer")
 
 
 
 
+async function init(){
+    async function loadMore(page,selector){
+        const moreButton=await page.$(selector)
+        if (moreButton){
+            console.log("more")
+            await moreButton.click()
+            await page.waitFor(selector,{timeout:5000}).catch(()=>{console.log("timeout")})
+            await loadMore(page,selector)
+        }
+        
+    }
+    
+    async function getComents(page,selector){
+        const coment=await page.$$eval(selector,commit=>commit.map(commit=>commit.innerText))
+        return coment
+    }
+   
+    const internetBrwser = await puppeteers.launch()
+    const page= await internetBrwser.newPage()
+    await page.goto("https://www.instagram.com/p/CDSLgg5nvL6/")
 
-const fakeCommits = ["juan","julio","jose","joao","juan"]
+    await loadMore(page,".dCJp8")
+    const comments=await getComents(page,".C4VMK h3")
+    console.log(comments)
+    const commitss=count(comments)
+
+    const winner=randomWinner(commitss)
+    console.log(winner)
+    await internetBrwser.close()
+
+}
+
+
+
+
 
 function count(commits){
     const counter=Object.assign(commits)
@@ -19,4 +53,4 @@ function randomWinner(count){
 
     
 }
-console.log(randomWinner(count(fakeCommits)))
+init()
